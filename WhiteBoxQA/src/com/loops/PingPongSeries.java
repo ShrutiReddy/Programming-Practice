f04e15f32dfd3f30618278b63c7f8feb7846ca62
ShrutiReddy <shruti@shrutis-mbp>
2015-12-16 13:45:26 -0800

    ping pong series

---
+++ WhiteBoxQA/src/com/loops/PingPongSeries.java
@@ -0,0 +1,29 @@
+package com.loops;
+
+import java.util.Scanner;
+
+public class PingPongSeries {
+int a;
+	/**
+	 * Write a program print "ping" if a number is divisible by 3,"pong" if a number is divisible by 5, and "ping pong"
+	  	 if number is divisible by both, else print the number.
+	 */
+	public static void main(String[] args) {
+		
+		for(int i =1;i<=100;i++){
+		if((i%3==0) && (i%5==0)){
+			System.out.println("ping pong");
+		}else if(i%5==0){
+			System.out.println("pong");
+				
+			}else if(i%3==0 ){
+				System.out.println("Ping ");
+			}else{System.out.println(i);
+		}
+	}
+		
+	}
+
+	}
+
+
