074267c3676955237686db45d14889895803a8f3
Your Nameshruti <shruti_reddy@yahoo.com>
2016-02-02 13:24:48 -0800

    Armstrongnumber code

---
+++ WhiteBoxQA/src/com/loops/ArmstrongNumber.java
@@ -0,0 +1,38 @@
+package com.loops;
+
+import java.util.Scanner;
+
+
+
+public class ArmstrongNumber {
+
+	
+	
+	public static void main(String[] args)  {
+		
+	
+		int sum=0;
+		int n, temp, r;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number to check if it is an armstrong number: ");
+		n= sc.nextInt();
+		
+		
+		 temp=n;
+		 
+		 while(temp!=0){
+			 r=temp%10;
+			 sum=sum+r*r*r;
+			 temp=temp/10;
+		 }
+		 if(n==sum){
+			 System.out.println(" Entered number is an armstrong number");
+		 }else{
+			 System.out.println("Entered number is not an armstrong number");
+		 }
+
+		
+		
+	}
+
+}
